2e7a5f15b90b64cecb0e66298e5fa300ac1b1f82
nermin <nermin@nermins-iMac.local>
2017-03-05 23:34:13 -0600

    Added new fields to Blog Post, modified Post Form

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,45 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
+from django import forms
 from .models import Post
 
 #Register Post model to admin site 
 #admin.site.register(Post)
+class PostAdminForm(forms.ModelForm):
+    class Meta:
+        model = Post
+
+        fields = [
+            'is_published',
+            'post_title',
+            'slug',
+            'excerpt',
+            'post_body',
+        ]
+        
+        labels = {
+            'is_published': 'Is Published?',
+        }
+    
 
 class PostAdmin(SummernoteModelAdmin):
     model = Post
-    
+
+    form = PostAdminForm
+
+    prepopulated_fields = {
+        'slug': ('post_title',)
+    }
+
+    def save_model(self, request, obj, form, change):
+        if form.has_changed() and 'is_published' in form.changed_data:
+            if obj.is_published:
+                obj.published_at = now()
+            else:
+                obj.published_at = None
+        
+        super().save_model(request, obj, form, change)
+
 admin.site.register(Post, PostAdmin)
 
 
--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,15 @@ class Post(models.Model):
     post_date = models.DateTimeField('date published')
     post_body = models.TextField()
     post_author = models.CharField(max_length=250, default='Nermin Kekic')
+    slug = models.SlugField(max_length=250, unique=True)
+    excerpt = models.TextField()
+    
+    created_at = models.DateTimeField('date created', auto_now_add=True )
+    published_at = models.DateTimeField('date published', null=True, blank=True)
+    updated_at = models.DateTimeField('last modified', auto_now=True)
+    is_published = models.BooleanField(default=False)
+    
+
 
     def __str__(self):
         return self.post_title
